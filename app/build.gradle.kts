e5aed6b633045bad5bcce1d432e06d5d19b64261
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-10-02 17:46:14 +0700

    Import coil library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.compose.material:material-icons-extended:1.5.2")
 
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
