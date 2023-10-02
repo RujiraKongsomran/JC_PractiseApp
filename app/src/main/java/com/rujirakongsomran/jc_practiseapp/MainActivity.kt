3b7e69a6d2924d6427ac9da6b5bf0d97c1e0ac4d
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-10-02 18:12:22 +0700

    Create LazyVerticalGrid and display photo

--- app/src/main/java/com/rujirakongsomran/jc_practiseapp/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_practiseapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.rujirakongsomran.jc_practiseapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
@@ -21,18 +23,17 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.rujirakongsomran.jc_practiseapp.model.Photo
+import com.rujirakongsomran.jc_practiseapp.repository.PhotoRepository
 import com.rujirakongsomran.jc_practiseapp.ui.theme.JC_PractiseAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,18 +42,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             JC_PractiseAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                SmallTopAppExample()
             }
         }
     }
 }
 
-@Preview(showSystemUi = true)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SmallTopAppExample() {
@@ -119,10 +114,24 @@ fun SmallTopAppExample() {
             )
         }
     ) { innerPadding ->
-        Text(
-            modifier = Modifier.padding(innerPadding),
-            text = "Example of a scaffold with a bottom app bar."
-        )
+        val photoRepository = PhotoRepository()
+        val getAllData = photoRepository.getAllData()
+        PhotoGrid(photos = getAllData, innerPadding)
+    }
+}
+
+@Composable
+fun PhotoGrid(
+    photos: List<Photo>,
+    paddingValues: PaddingValues
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        contentPadding = paddingValues
+    ) {
+        items(photos) { photo ->
+            PhotoItem(photo = photo)
+        }
     }
 }
 
